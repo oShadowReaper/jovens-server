9a162db548f8438ab65482cd993005bb16757d56
GabrielBasitt <gabriel_basitt@estudante.sesisenai.org.br>
2022-07-12 15:28:40 -0300

    aa

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 5432
 const routes = require('./src/queries')
 const cors = require('cors')
 
@@ -18,10 +18,14 @@ app.get('/',(request,response) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`server is runing in https://jovens-db.herokuapp.com:${PORT}`)
+  console.log(`server is runing in https://jovens-db.herokuapp.com`)
 })
 
 app.get('/pessoa', routes.getPessoa)
 app.get('/pessoa/:id', routes.getPessoaById)
 app.post('/pessoa', routes.createPessoa)
 app.put('/pessoa/:id', routes.updatePessoa)
+aapp.get('/empresa', routes.getEmpresa)
+app.get('/empresa/:id', routes.getEmpresaById)
+app.post('/empresa', routes.createEmpresa)
+app.put('/empresa/:id', routes.updateEmpresa)
--- src/queries.js
+++ src/queries.js
@@ -1,3 +1,5 @@
+const { response } = require('express');
+
 const Pool = require('pg').Pool
 require("dotenv").config()
 
@@ -8,6 +10,7 @@ const db = new Pool({
     connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig
 })
+
 
 const getPessoa = (request, response) => {
   db.query('SELECT * FROM pessoa ORDER BY nome_completo ASC',
@@ -38,6 +41,9 @@ const createPessoa = (request, response) => {
       [nome_completo, cpf, telefone, data_nascimento, email, senha], (error, results) => {
     if (error){
       throw error
+    }else{
+      response.status(201).send('Cadastro concluido')
+
     }
     response.status(201).send('Cadastro concluido')
   })
@@ -55,9 +61,129 @@ const updatePessoa = (request, response) => {
   })
 }
 
+const getEmpresa = (request, response) => {
+  db.query('SELECT * FROM empresa ORDER BY nome_empresa ASC',
+        (error, results) =>{
+          if (error) {
+            throw error
+          }
+          response.status(200).json(results.rows)
+        })
+  
+}
+const getEmpresaById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  db.query('SELECT * FROM empresa WHERE id = $1', [id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+}
+
+const createEmpresa = (request, response) => {
+  const {nome_empresa, cnpj, telefone, data_fundacao, email, senha} = request.body
+
+    db.query('INSERT INTO empresa(nome_empresa, cnpj, telefone, data_fundacao, email, senha)VALUES($1, $2, $3, $4, $5, $6)',
+      [nome_empresa, cnpj, telefone, data_fundacao, email, senha], (error, results) => {
+    if (error){
+      throw error
+    }else{
+      response.status(201).send('Cadastro concluido')
+
+    }
+    response.status(201).send('Cadastro concluido')
+  })
+}
+const updateEmpresa = (request, response) => {  
+  const id = parseInt(request.params.id)
+  const {nome_empresa, idade, cidade} = request.body
+
+  db.query('UPDATE empresa set nome_empresa = $1, idade = $2, cidade = $3 where id = $4',
+  [nome_empresa, idade, cidade, id], (error, results) => {
+    if(error) {
+      throw error
+    }
+    response.status(201).send('perfil atualizado')
+  })
+}
+const getPostagem = (request, response) => {
+  db.query('SELECT * FROM postagem ORDER BY idpostagem DESC',
+        (error, results) =>{
+          if (error) {
+            throw error
+          }
+          response.status(200).json(results.rows)
+        })
+  
+}
+const getPostagemById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  db.query('SELECT * FROM postagem WHERE id = $1', [id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+}
+
+const createPostagem = (request, response) => {
+  const {foto, data_postagem, titulo, descricao} = request.body
+
+    db.query('INSERT INTO postagem(foto, data_postagem, titulo, descricao)VALUES($1, $2, $3, $4)',
+      [foto, data_postagem, titulo, descricao ], (error, results) => {
+    if (error){
+      throw error
+    }else{
+      response.status(201).send('Post concluido')
+
+    }
+    response.status(201).send('Post concluido')
+  })
+}
+const updatePostagem = (request, response) => {
+  const id = parseInt(request.params.id)
+  const {foto,data_postagem, titulo, descricao} = request.body
+
+  db.query('UPDATE postagem set foto = $1,  data_postagem = $2, titulo = $3 descricao id = $4',
+  [foto, data_postagem, titulo, descricao], (error, results) => {
+    if(error) {
+      throw error
+    }
+    response.status(201).send('post atualizado')
+  })
+}
+
+const deletePostagem = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  db.query('DELETE FROM postagem WHERE id = $1', [id],
+      (error, results) => {
+        if(error){
+          throw error
+        }
+        response.status(201).send('Postagem excluida')
+      })
+    }
+
+
+
 module.exports = {
   getPessoa,
   getPessoaById,
   createPessoa,
-  updatePessoa
+  updatePessoa,
+  getEmpresa,
+  getEmpresaById,
+  createEmpresa,
+  updateEmpresa,
+  getPostagem,
+  getPostagemById,
+  createPostagem,
+  updatePostagem,
+  deletePostagem
 }
